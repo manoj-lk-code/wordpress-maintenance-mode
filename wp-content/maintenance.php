d886a09a44c1fac07de8f2cda0a9d426c47f2dd2
Manj (WPzonify) <81701675+manoj-wpzonify@users.noreply.github.com>
2021-09-09 09:56:45 +0530

    Add files via upload

---
+++ wp-content/maintenance.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Segoe UI, sans-serif; color: #000000; background-color: #f6f8fa; }
+  article { text-align: left; width: 650px; margin: 0 auto; padding: 20px; box-shadow: -1px 0px 97px -38px rgb(198 182 182 / 70%); border-radius: 10px; background-color: #fff;}
+</style>
+
+<article>
+    <h2>Updating website to better serve you.!</h2>
+    <div>
+        <p>We apologise for the inconvenience, but we are now undergoing maintenance. Thank you for your patience. Please visit us after a while.</p>
+        <p>- Team</p>
+    </div>
+</article>
