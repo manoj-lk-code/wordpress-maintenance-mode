0784e4555a7da8bb33768af541d62560ffe13564
Manj (WPzonify) <81701675+manoj-wpzonify@users.noreply.github.com>
2021-09-10 11:02:52 +0530

    Update maintenance.php

--- wp-content/maintenance.php
+++ wp-content/maintenance.php
@@ -1,12 +1,19 @@
 <!doctype html>
 <title>Site Maintenance</title>
+<head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <meta http-equiv="Cache-control" content="no-cache">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=1">
 <style>
   body { text-align: center; padding: 150px; }
   h1 { font-size: 50px; }
   body { font: 18px Segoe UI, sans-serif; color: #000000; background-color: #f6f8fa; }
   article { text-align: left; width: 650px; margin: 0 auto; padding: 20px; box-shadow: -1px 0px 97px -38px rgb(198 182 182 / 70%); border-radius: 10px; background-color: #fff;}
 </style>
-
+</head>
+<body>
 <article>
     <h2>Updating website to better serve you.!</h2>
     <div>
@@ -14,3 +21,5 @@
         <p>- Team</p>
     </div>
 </article>
+</body>
+</html>
