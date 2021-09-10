b09b7a301216a6f46145b117f6328bfdafc36f2b
Manj (WPzonify) <81701675+manoj-wpzonify@users.noreply.github.com>
2021-09-10 11:33:04 +0530

    Update maintenance.php

--- wp-content/maintenance.php
+++ wp-content/maintenance.php
@@ -1,5 +1,8 @@
 <?php
- http_response_code(503)
+ http_response_code(503);
+ header('Cache-Control: no-store, no-cache, must-revalidate');
+ header('Cache-Control: post-check=0, pre-check=0', false);
+ header('Pragma: no-cache');
 ?>
 <!doctype html>
 
