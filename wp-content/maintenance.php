df2927df38c9063e6908a1e53d8f81557d24cc3f
Manj (WPzonify) <81701675+manoj-wpzonify@users.noreply.github.com>
2021-09-10 11:08:37 +0530

    Update maintenance.php

--- wp-content/maintenance.php
+++ wp-content/maintenance.php
@@ -1,18 +1,23 @@
+<?php
+ http_response_code(503)
+?>
 <!doctype html>
-<title>Site Maintenance</title>
+
 <head>
+    <title>Site Maintenance</title>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <meta http-equiv="Cache-control" content="no-cache">
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Expires" content="0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=1">
 <style>
-  body { text-align: center; padding: 150px; }
-  h1 { font-size: 50px; }
+  body { text-align: center; padding-top: 150px; }
   body { font: 18px Segoe UI, sans-serif; color: #000000; background-color: #f6f8fa; }
   article { text-align: left; width: 650px; margin: 0 auto; padding: 20px; box-shadow: -1px 0px 97px -38px rgb(198 182 182 / 70%); border-radius: 10px; background-color: #fff;}
+  p {font: 18px Segoe UI, sans-serif;}
 </style>
 </head>
+
 <body>
 <article>
     <h2>Updating website to better serve you.!</h2>
@@ -20,6 +25,7 @@
         <p>We apologise for the inconvenience, but we are now undergoing maintenance. Thank you for your patience. Please visit us after a while.</p>
         <p>- Team</p>
     </div>
-</article>
-</body>
+</article>    
+</body
+    
 </html>
